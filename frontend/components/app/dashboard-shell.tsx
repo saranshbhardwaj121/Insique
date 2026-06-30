6f21345e41dae82909fa0d509c113738e0c9de2f
Saransh Bhardwaj <saranshbhardwaj121@gmail.com>
2026-06-30 18:30:17 +0530

    mobile view

--- frontend/components/app/dashboard-shell.tsx
+++ frontend/components/app/dashboard-shell.tsx
@@ -22,7 +22,7 @@ export function DashboardShell({ children }: DashboardShellProps) {
 
       <div className="flex flex-1 flex-col overflow-hidden">
         <TopBar />
-        <main className="flex-1 overflow-y-auto p-4 lg:p-6">
+        <main className="flex-1 overflow-x-hidden overflow-y-auto p-4 lg:p-6">
           {children}
         </main>
       </div>
